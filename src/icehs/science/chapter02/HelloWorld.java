b9e541a45ccdc064e4825b68ebcd6b8480bc4133
user <user@10.138.6.200>
2021-05-11 22:08:24 +0900

    Chapter02

---
+++ src/icehs/science/chapter02/HelloWorld.java
@@ -0,0 +1,10 @@
+package icehs.science.chapter02;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("자바를 자바라"); //sysout 후에 Ctrl + Space
+	}
+
+}
