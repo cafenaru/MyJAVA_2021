e0c4b9803ceb165dac7405f5adef9fd4fdb45e1f
user <user@10.138.6.200>
2021-05-11 22:15:37 +0900

    Chapter02

--- src/icehs/science/chapter02/HelloWorld.java
+++ src/icehs/science/chapter02/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("자바를 자바라"); //sysout 후에 Ctrl + Space
+		System.out.println("운서동 살기 좋아요...");
 	}
 
 }
